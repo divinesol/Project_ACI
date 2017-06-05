cf0995b9975b33c1e52ae9604906b0f8a7685a0a
Jason <deathdragon520@hotmail.com>
2017-06-05 14:39:54 +0800

    Changed old UI to new UI

--- Assets/Scripts/StocknPopularityManager.cs
+++ Assets/Scripts/StocknPopularityManager.cs
@@ -7,15 +7,17 @@ public class StocknPopularityManager : MonoBehaviour {
     public static float stockValue;
     public static float popValue;
     public static float mainRatingValue;
- 
-    public Image stockBar;
+
+    public Text stockText;
+
     public Image popularityBar;
     public Image mainRatingBar;
     public Image popIcon;
 
     // Use this for initialization
     void Start () {
-        stockBar.fillAmount = 0.0f;
+
+        stockText.text = (stockValue * 100).ToString() + "%";
         popularityBar.fillAmount = 0.0f;
         mainRatingBar.fillAmount = 0.0f;
     }
@@ -33,43 +35,44 @@ public class StocknPopularityManager : MonoBehaviour {
         if (popValue <= 0) popValue = 0;
         if (mainRatingValue <= 0) mainRatingValue = 0;
 
-        stockBar.fillAmount = stockValue;
         popularityBar.fillAmount = popValue;
         mainRatingBar.fillAmount = mainRatingValue;
 
-        if(stockValue >= 0 && stockValue <= 0.2)
-            stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_0");
-        if (stockValue > 0.2 && stockValue <= 0.4)                                   
-            stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_1");
-        if (stockValue > 0.4 && stockValue <= 0.6)                               
-            stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_2");
-        if (stockValue > 0.6 && stockValue <= 0.8)                                 
-            stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_3");
-        if (stockValue > 0.8 && stockValue <= 1)                               
-            stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_4");
+        stockText.text = (stockValue * 100).ToString() + "%";
+
+        //if(stockValue >= 0 && stockValue <= 0.2)
+        //    stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_0");
+        //if (stockValue > 0.2 && stockValue <= 0.4)                                   
+        //    stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_1");
+        //if (stockValue > 0.4 && stockValue <= 0.6)                               
+        //    stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_2");
+        //if (stockValue > 0.6 && stockValue <= 0.8)                                 
+        //    stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_3");
+        //if (stockValue > 0.8 && stockValue <= 1)                               
+        //    stockBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/stock_4");
 
 
-        if (popValue >= 0 && popValue <= 0.2)
-            popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_0");
-        if (popValue > 0.2 && popValue <= 0.4)                                                 
-            popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_1");
-        if (popValue > 0.4 && popValue <= 0.6)                                                
-            popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_2");
-        if (popValue > 0.6 && popValue <= 0.8)                                                
-            popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_3");
-        if (popValue > 0.8 && popValue <= 1)                                                  
-            popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_4");
+        //if (popValue >= 0 && popValue <= 0.2)
+        //    popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_0");
+        //if (popValue > 0.2 && popValue <= 0.4)                                                 
+        //    popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_1");
+        //if (popValue > 0.4 && popValue <= 0.6)                                                
+        //    popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_2");
+        //if (popValue > 0.6 && popValue <= 0.8)                                                
+        //    popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_3");
+        //if (popValue > 0.8 && popValue <= 1)                                                  
+        //    popularityBar.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/Popularity_4");
 
-        if (mainRatingValue >= 0 && mainRatingValue <= 0.2)
-            popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_1");
-        if (mainRatingValue > 0.2 && mainRatingValue <= 0.4)                      
-            popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_2");
-        if (mainRatingValue > 0.4 && mainRatingValue <= 0.6)                     
-            popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_3");
-        if (mainRatingValue > 0.6 && mainRatingValue <= 0.8)                     
-            popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_4");
-        if (mainRatingValue > 0.8 && mainRatingValue <= 1)                       
-            popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_5");
+        //if (mainRatingValue >= 0 && mainRatingValue <= 0.2)
+        //    popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_1");
+        //if (mainRatingValue > 0.2 && mainRatingValue <= 0.4)                      
+        //    popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_2");
+        //if (mainRatingValue > 0.4 && mainRatingValue <= 0.6)                     
+        //    popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_3");
+        //if (mainRatingValue > 0.6 && mainRatingValue <= 0.8)                     
+        //    popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_4");
+        //if (mainRatingValue > 0.8 && mainRatingValue <= 1)                       
+        //    popIcon.sprite = Resources.Load<Sprite>("HUD_Popularity and stock/face_5");
 
 
         #region DebugInputs
