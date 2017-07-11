02b4c9b9fb64c4a5ad7f049230990cecb30dd99b
Jason <deathdragon520@hotmail.com>
2017-07-11 18:03:15 +0800

    Added test code for meat fabrication

--- Assets/Scripts/MeatFabManager.cs
+++ Assets/Scripts/MeatFabManager.cs
@@ -3,15 +3,57 @@ using System.Collections;
 
 public class MeatFabManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public static MeatFabManager Instance;
+
+    public enum MEAT_CUT_TYPE
+    {
+        CHICKEN_MAIN,
+        CHICKEN_THIGH,
+        BEEF_TEST
+
+    };
+
+    MEAT_CUT_TYPE meatCutTypes;
+
+    Vector3 startCutPoint, endCutPoint;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        CheckIfCutIsCorrect();
+    }
 
+    public void CheckIfCutIsCorrect()
+    {
+        switch (meatCutTypes)
+        {
+            case MEAT_CUT_TYPE.BEEF_TEST:
+                break;
 
+            case MEAT_CUT_TYPE.CHICKEN_MAIN:
+                break;
+
+            case MEAT_CUT_TYPE.CHICKEN_THIGH:
+                break;
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if (hit != false && hit.collider != null)
+        {
+            Debug.Log("object clicked: " + hit.collider.tag);
+        }
+    }
 }
