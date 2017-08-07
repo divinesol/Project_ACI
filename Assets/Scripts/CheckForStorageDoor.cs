eb2a99e190406bc7ee31b3fbf5fc3acda4389f49
ZhiYuan <esrielthefallen@gmail.com>
2017-08-07 17:17:00 +0800

    .

---
+++ Assets/Scripts/CheckForStorageDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckForStorageDoor : MonoBehaviour {
+
+    public  int num = -1;
+	void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("collllllllll");
+        switch (col.gameObject.tag)
+        {
+            case "dryDoor":
+                num = 0;
+                break;
+            case "wetDoor":
+                num = 1;
+                break;
+            case "freezeDoor":
+                num = 2;
+                break;
+        }
+    }
+}
